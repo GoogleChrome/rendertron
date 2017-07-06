d73546e7568eb6d61d89ceb4b4e49d964bcd862e
Sam li <samli@google.com>
2017-07-06 15:09:19 -0700

    Add basic tests for the server

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 A Docker container which runs headless Chrome and renders web pages on the fly.
 
 ## Dependencies
-This project requires Node 7+ and Docker ([installation instructions](https://docs.docker.com/engine/installation/)).
+This project requires Node 7+ and Docker ([installation instructions](https://docs.docker.com/engine/installation/)). For deployment this
+project uses the [Google Cloud Platform SDK](https://cloud.google.com/sdk/).
 
 ## Installing
 Install node dependencies using:
@@ -65,4 +66,9 @@ docker kill bot-render-container
 Clear containers:
 ```bash
 docker rm -f $(docker ps -a -q)
+```
+
+## Deploying to Google Cloud Platform
+```
+gcloud app deploy app.yaml --project <your-project-id>
 ```
--- src/main.js
+++ src/main.js
@@ -13,14 +13,16 @@ app.get('/', async function(request, response) {
 
 app.get('/_ah/health', (request, response) => response.send('OK'));
 
-const port = process.env.PORT || '3000';
-
-// Should respond to _ah/health _ah/start _ah/stop
-
-if (!startChromium()) {
-  console.error('Failed to start Chromium');
-} else {
+// Don't open a port when running from inside a module (eg. tests). Importing
+// module can control this.
+const chromiumStarted = startChromium();
+if (chromiumStarted && !module.parent) {
+  const port = process.env.PORT || '3000';
   app.listen(port, function() {
     console.log('Listening on port', port);
   });
+} else if (!chromiumStarted) {
+  console.error('Failed to start Chromium');
 }
+
+module.exports = app;
