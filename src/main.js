5b2b9947ba0543a421e908824e5faadabe1b59fc
Sam li <samli@google.com>
2017-07-07 17:06:29 -0700

    Use Chrome launcher

--- src/main.js
+++ src/main.js
@@ -1,28 +1,30 @@
 'use strict';
 
-const Renderer = require('./renderer');
-const startChromium = require('./chromium');
+const render = require('./renderer');
+const chromeLauncher = require('chrome-launcher');
 const express = require('express');
 
 const app = express();
 
 app.get('/', async function(request, response) {
-  const head = await new Renderer(request.query.url).extractHead().catch((err) => console.error(err));
+  const head = await render(request.query.url).catch((err) => console.error(err));
   response.send(head);
 });
 
 app.get('/_ah/health', (request, response) => response.send('OK'));
 
-// Don't open a port when running from inside a module (eg. tests). Importing
-// module can control this.
-const chromiumStarted = startChromium();
-if (chromiumStarted && !module.parent) {
+chromeLauncher.launch({
+  chromeFlags: ['--headless', '--disable-gpu', '--remote-debugging-address=0.0.0.0'],
+  port: 9222
+}).then(chrome => {
+  // Don't open a port when running from inside a module (eg. tests). Importing
+  // module can control this.
   const port = process.env.PORT || '3000';
-  app.listen(port, function() {
-    console.log('Listening on port', port);
-  });
-} else if (!chromiumStarted) {
-  console.error('Failed to start Chromium');
-}
+  if (!module.parent) {
+    app.listen(port, function() {
+      console.log('Listening on port', port);
+    });
+  }
+});
 
 module.exports = app;
