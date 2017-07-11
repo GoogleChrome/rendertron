915d587c922212c045ba8d3754b82a1fff48ad2d
Sam li <samli@google.com>
2017-07-11 15:32:30 -0700

    Wait for chrome to launch before running tests

--- src/renderer.js
+++ src/renderer.js
@@ -4,6 +4,7 @@ const CDP = require('chrome-remote-interface');
 
 function extractHead(url) {
   return new Promise((resolve, reject) => {
+    debugger;
     CDP.New().then((tab) => {
       return CDP({tab: tab});
     }).then((client) => {
--- test/app-test.js
+++ test/app-test.js
@@ -9,19 +9,22 @@ const test = require('ava');
  * the server, allowing for a clean state between each test.
  * @return {!Object} app server
  */
-function createServer() {
+async function createServer() {
   delete require.cache[require.resolve('../src/main.js')];
-  return require('../src/main.js');
+  const app = await require('../src/main.js');
+  return app;
 }
 
 test('health check responds correctly', async(t) => {
-  const res = await request(createServer()).get('/_ah/health');
+  const app = await createServer();
+  const res = await request(app).get('/_ah/health');
   t.is(res.status, 200);
 });
 
 test('renders basic script', async(t) => {
+  const app = await createServer();
   const testFile = path.resolve(__dirname, 'resources/basic-script.html');
-  const res = await request(createServer()).get('/?url=file://' + testFile);
+  const res = await request(app).get('/?url=file://' + testFile);
   t.is(res.status, 200);
   t.is(res.text.replace(/\n/, ''), '<head>my head element</head>');
 });
