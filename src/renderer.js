51ab1b34751f01d1bc9e4d1fc16a2bd8b7d86567
Sam Li <samli@google.com>
2017-07-10 20:59:13 -0700

    Use Chrome launcher (#9)

--- src/renderer.js
+++ src/renderer.js
@@ -2,53 +2,47 @@
 
 const CDP = require('chrome-remote-interface');
 
-class Renderer {
-  constructor(url) {
-    this._url = url;
-  }
+function extractHead(url) {
+  return new Promise((resolve, reject) => {
+    CDP.New().then((tab) => {
+      return CDP({tab: tab});
+    }).then((client) => {
+      try {
+        // Extract DevTools domains.
+        const {Page, Runtime, Network} = client;
 
-  extractHead() {
-    return new Promise((resolve, reject) => {
-      CDP.New().then((tab) => {
-        return CDP({tab: tab});
-      }).then((client) => {
-        try {
-          // Extract DevTools domains.
-          const {Page, Runtime, Network} = client;
+        // Enable events on domains we are interested in.
+        Promise.all([
+          Page.enable(),
+          Runtime.enable(),
+          Network.enable(),
+        ]).then(() => {
+          return Promise.all([
+            Network.clearBrowserCache(),
+            Network.setCacheDisabled({cacheDisabled: true}),
+            Network.setBypassServiceWorker({bypass: true}),
+          ]);
+        }).then(() => {
+          Page.navigate({url: url});
+        });
 
-          // Enable events on domains we are interested in.
-          Promise.all([
-            Page.enable(),
-            Runtime.enable(),
-            Network.enable(),
-          ]).then(() => {
-            return Promise.all([
-              Network.clearBrowserCache(),
-              Network.setCacheDisabled({cacheDisabled: true}),
-              Network.setBypassServiceWorker({bypass: true}),
-            ]);
-          }).then(() => {
-            Page.navigate({url: this._url});
-          });
-
-          // Load and dump DOM of head element.
-          Page.loadEventFired(() => {
-            setTimeout(async() => {
-              let result = await Runtime.evaluate({expression: 'document.head.outerHTML'});
-              CDP.Close({id: client.tab.id});
-              resolve(result.result.value);
-            }, 1500);
-          });
-        } catch (err) {
-          console.error(err);
-          CDP.Close({id: client.tab.id});
-          reject(err);
-        }
-      }).catch((e) => {
-        reject(e);
-      });
+        // Load and dump DOM of head element.
+        Page.loadEventFired(() => {
+          setTimeout(async() => {
+            let result = await Runtime.evaluate({expression: 'document.head.outerHTML'});
+            CDP.Close({id: client.tab.id});
+            resolve(result.result.value);
+          }, 1500);
+        });
+      } catch (err) {
+        console.error(err);
+        CDP.Close({id: client.tab.id});
+        reject(err);
+      }
+    }).catch((e) => {
+      reject(e);
     });
-  }
+  });
 }
 
-module.exports = Renderer;
+module.exports = extractHead;
