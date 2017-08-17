cc33d68331b728d8ef2217034bf7ef91418aa858
Alexander Marks <aomarks@google.com>
2017-08-17 15:45:29 -0700

    Add Express middleware. (#57)

---
+++ middleware/src/middleware.js
@@ -0,0 +1,93 @@
+'use strict';
+
+const request = require('request');
+
+/**
+ * A default set of user agent patterns for bots/crawlers that do not perform
+ * well with pages that require JavaScript.
+ */
+const botUserAgents = module.exports.botUserAgents = [
+  'W3C_Validator',
+  'baiduspider',
+  'bingbot',
+  'embedly',
+  'facebookexternalhit',
+  'linkedinbo',
+  'outbrain',
+  'pinterest',
+  'quora link preview',
+  'rogerbo',
+  'showyoubot',
+  'slackbot',
+  'twitterbot',
+  'vkShare',
+];
+
+/* eslint-disable no-multi-spaces */
+
+/**
+ * A default set of file extensions for static assets that do not need to be
+ * proxied.
+ */
+const staticFileExtensions = [
+  'ai',  'avi',  'css', 'dat',  'dmg', 'doc',     'doc',  'exe', 'flv',
+  'gif', 'ico',  'iso', 'jpeg', 'jpg', 'js',      'less', 'm4a', 'm4v',
+  'mov', 'mp3',  'mp4', 'mpeg', 'mpg', 'pdf',     'png',  'ppt', 'psd',
+  'rar', 'rss',  'svg', 'swf',  'tif', 'torrent', 'ttf',  'txt', 'wav',
+  'wmv', 'woff', 'xls', 'xml',  'zip',
+];
+
+/* eslint-enable no-multi-spaces */
+
+/**
+ * @return {!Object} A new Express middleware function that proxies requests to
+ * a Rendertron bot rendering service. Options:
+ *
+ * @param {!Object} options Configuration object with the following properties:
+ * - proxyUrl: Base URL of the Rendertron proxy service. Required.
+ * - userAgentPattern: Regular expression to match user agent to proxy.
+ *   Defaults to a set of bots that do not perform well with pages that require
+ *   JavaScript.
+ * - excludeUrlPattern: Regular expression used to exclude request URL paths.
+ *   Defaults to a set of typical static asset file extensions.
+ * - injectShadyDom: Force web components polyfills to be loaded and enabled.
+ * - timeout: Millisecond timeout for proxy requests.
+ */
+module.exports.makeMiddleware = function(options) {
+  if (!options || !options.proxyUrl) {
+    throw new Error('Must set options.proxyUrl.');
+  }
+  let proxyUrl = options.proxyUrl;
+  if (!proxyUrl.endsWith('/')) {
+    proxyUrl += '/';
+  }
+  const userAgentPattern =
+      options.userAgentPattern || new RegExp(botUserAgents.join('|'), 'i');
+  const excludeUrlPattern = options.excludeUrlPattern ||
+      new RegExp(`\\.(${staticFileExtensions.join('|')})$`, 'i');
+  const injectShadyDom = !!options.injectShadyDom;
+  // The Rendertron service itself has a hard limit of 10 seconds to render, so
+  // let's give a little more time than that by default.
+  const timeout = options.timeout || 11000; // Milliseconds.
+
+  return function rendertronMiddleware(req, res, next) {
+    if (!userAgentPattern.test(req.headers['user-agent']) ||
+        excludeUrlPattern.test(req.path)) {
+      next();
+      return;
+    }
+    const incomingUrl =
+        req.protocol + '://' + req.get('host') + req.originalUrl;
+    let renderUrl = proxyUrl + encodeURIComponent(incomingUrl);
+    if (injectShadyDom) {
+      renderUrl += '?wc-inject-shadydom';
+    }
+    request({url: renderUrl, timeout}, (e) => {
+      if (e) {
+        console.error(
+            `[rendertron middleware] ${e.code} error fetching ${renderUrl}`);
+        next();
+      }
+    }).pipe(res);
+  };
+};
---
+++ middleware/test/middleware-test.js
@@ -0,0 +1,169 @@
+'use strict';
+
+const express = require('express');
+const supertest = require('supertest');
+const test = require('ava');
+
+const rendertron = require('../src/middleware');
+
+/**
+ * Start the given Express app on localhost with a random port.
+ * @param {!Object} app The app.
+ * @return {Promise<string>}Promise of the URL.
+ */
+async function listen(app) {
+  return new Promise((resolve) => {
+    const server = app.listen(/* random */ 0, 'localhost', () => {
+      resolve(`http://localhost:${server.address().port}`);
+    });
+  });
+}
+
+/**
+ * Make an Express app that uses the Rendertron middleware and returns
+ * "fallthrough" if the middleware skipped the request (i.e. called `next`).
+ * @param {Object} options Rendertron middleware options.
+ * @return {!Object} The app.
+ */
+function makeApp(options) {
+  return express()
+      .use(rendertron.makeMiddleware(options))
+      .use((req, res) => res.end('fallthrough'));
+}
+
+/**
+ * Make an Express app that takes the place of a Rendertron server instance and
+ * always responds with "proxy <decoded url>".
+ * @return {!Object} The app.
+ */
+function makeProxy() {
+  return express().use((req, res) => {
+    res.end('proxy ' + decodeURIComponent(req.url.substring(1)));
+  });
+}
+
+const bot = 'slackbot';
+const human = 'Chrome';
+
+/**
+ * GET a URL with the given user agent.
+ * @param {string} userAgent The user agent string.
+ * @param {string} host The host part of the URL.
+ * @param {string} path The path part of the URL.
+ * @return {Promise<!Object>} Promise of the GET response.
+ */
+async function get(userAgent, host, path) {
+  return await supertest(host).get(path).set('User-Agent', userAgent);
+}
+
+test('makes a middleware function', async (t) => {
+  const m = rendertron.makeMiddleware({proxyUrl: 'http://example.com'});
+  t.truthy(m);
+});
+
+test('throws if no proxyUrl given', async (t) => {
+  t.throws(() => rendertron.makeMiddleware());
+  t.throws(() => rendertron.makeMiddleware({}));
+  t.throws(() => rendertron.makeMiddleware({proxyUrl: ''}));
+});
+
+test('proxies through given url', async (t) => {
+  const proxyUrl = await listen(makeProxy());
+  const appUrl = await listen(makeApp({proxyUrl}));
+
+  const res = await get(bot, appUrl, '/foo');
+  t.is(res.status, 200);
+  t.is(res.text, 'proxy ' + appUrl + '/foo');
+});
+
+test('proxyUrl can have trailing slash', async (t) => {
+  const proxyUrl = await listen(makeProxy());
+  // Make sure our other tests are testing the no-trailing-slash case.
+  t.false(proxyUrl.endsWith('/'));
+  const appUrl = await listen(makeApp({proxyUrl: proxyUrl + '/'}));
+
+  const res = await get(bot, appUrl, '/foo');
+  t.is(res.status, 200);
+  t.is(res.text, 'proxy ' + appUrl + '/foo');
+});
+
+test('adds shady dom parameter', async (t) => {
+  const proxyUrl = await listen(makeProxy());
+  const appUrl = await listen(makeApp({proxyUrl, injectShadyDom: true}));
+
+  const res = await get(bot, appUrl, '/foo');
+  t.is(res.status, 200);
+  t.is(res.text, 'proxy ' + appUrl + '/foo?wc-inject-shadydom');
+});
+
+test('excludes static file paths by default', async (t) => {
+  const proxyUrl = await listen(makeProxy());
+  const appUrl = await listen(makeApp({proxyUrl}));
+
+  const res = await get(bot, appUrl, '/foo.png');
+  t.is(res.text, 'fallthrough');
+});
+
+test('url exclusion only matches url path component', async (t) => {
+  const proxyUrl = await listen(makeProxy());
+  const appUrl = await listen(makeApp({proxyUrl}));
+
+  const res = await get(bot, appUrl, '/foo.png?params');
+  t.is(res.text, 'fallthrough');
+});
+
+test('excludes non-bot user agents by default', async (t) => {
+  const proxyUrl = await listen(makeProxy());
+  const appUrl = await listen(makeApp({proxyUrl}));
+
+  const res = await get(human, appUrl, '/foo');
+  t.is(res.text, 'fallthrough');
+});
+
+test('respects custom user agent pattern', async (t) => {
+  const proxyUrl = await listen(makeProxy());
+  const appUrl = await listen(makeApp({proxyUrl, userAgentPattern: /borg/}));
+
+  let res;
+
+  res = await get('humon', appUrl, '/foo');
+  t.is(res.text, 'fallthrough');
+
+  res = await get('borg', appUrl, '/foo');
+  t.is(res.text, 'proxy ' + appUrl + '/foo');
+});
+
+test('respects custom exclude url pattern', async (t) => {
+  const proxyUrl = await listen(makeProxy());
+  const appUrl = await listen(makeApp({proxyUrl, excludeUrlPattern: /foo/}));
+
+  let res;
+
+  res = await get(bot, appUrl, '/foo');
+  t.is(res.text, 'fallthrough');
+
+  res = await get(bot, appUrl, '/bar');
+  t.is(res.text, 'proxy ' + appUrl + '/bar');
+});
+
+test('forwards proxy error status and body', async (t) => {
+  // This proxy always returns an error.
+  const proxyUrl = await listen(
+      express().use((req, res) => res.status(500).end('proxy error')));
+  const appUrl = await listen(makeApp({proxyUrl}));
+
+  const res = await get(bot, appUrl, '/bar');
+  t.is(res.status, 500);
+  t.is(res.text, 'proxy error');
+});
+
+test('falls through after timeout', async (t) => {
+  // This proxy returns after 20ms, but our timeout is 10ms.
+  const proxyUrl = await listen(express().use((req, res) => {
+    setTimeout(() => res.end('too slow'), 20);
+  }));
+  const appUrl = await listen(makeApp({proxyUrl, timeout: 10}));
+
+  const res = await get(bot, appUrl, '/foo');
+  t.is(res.text, 'fallthrough');
+});
