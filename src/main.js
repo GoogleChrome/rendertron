c45af41874dff6bb892374fa1d8386d9e523802c
Sam Li <samli@google.com>
2018-02-01 14:24:11 -0800

    Potential fix

--- src/main.js
+++ src/main.js
@@ -157,7 +157,7 @@ app.stop = async() => {
 };
 
 const appPromise = chromeLauncher.launch({
-  chromeFlags: ['--headless', '--disable-gpu', '--remote-debugging-address=0.0.0.0'],
+  chromeFlags: ['--headless', '--disable-gpu'],
   port: 0
 }).then((chrome) => {
   console.log('Chrome launched with debugging on port', chrome.port);
