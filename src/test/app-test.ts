c3ac01170a00302f1becf285b7f64cc7143c26c5
martinsplitt <martin@geekonaut.de>
2018-08-27 16:12:25 +0200

    Adds test for JSON-LD

--- src/test/app-test.ts
+++ src/test/app-test.ts
@@ -92,6 +92,16 @@ test('script tags and link[rel=import] tags are stripped', async (t) => {
   t.true(res.text.indexOf('element-text') != -1);
 });
 
+test('script tags for JSON-LD are not stripped', async (t) => {
+  const res = await server.get(`/render/${testBase}include-json-ld.html`);
+  t.is(res.status, 200);
+  t.false(res.text.indexOf('script src') != -1);
+  t.true(res.text.indexOf('injectedElement') != -1);
+  t.false(res.text.indexOf('link rel') != -1);
+  t.true(res.text.indexOf('element-text') != -1);
+  t.true(res.text.indexOf('application/ld+json') != -1);
+});
+
 test('server status code should be forwarded', async (t) => {
   const res = await server.get('/render/http://httpstat.us/404');
   t.is(res.status, 404);
