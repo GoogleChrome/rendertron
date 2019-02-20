813d4b83d91f30d52244b375a12632909df64c8f
Himanshu Pandey <himanshu@gozefo.com>
2019-02-20 17:10:01 +0530

    add more test cases for browser pool

--- src/test/browserPool-test.ts
+++ src/test/browserPool-test.ts
@@ -26,3 +26,63 @@ test('after max use browser is disposed', async (t) => {
     t.true(browser1 !== browser3);
 });
 
+
+test('number of browsers would not exceed max value', async (t) => {
+    const browserPool = new BrowserPool({
+        browserMaxUse: 2,
+        poolSettings: { // options for generic pool
+            idleTimeoutMillis: 300000,
+            max: 1,
+            min: 1,
+            testOnBorrow: true,
+        },
+        browserArgs: {args: ['--no-sandbox']}
+    });
+    // console.log('browserPool.pool.size', browserPool.pool.size);
+    let browserCount = 0;
+
+    await browserPool.acquire(async() => {
+        browserCount ++;
+
+        const delay = new Promise(function(resolve) {
+            setTimeout(function() {
+                resolve();
+            }, 1000);
+        });
+        await Promise.race([
+            // because max is one and one browser is acquire next aquire would not be resolved because of browserMaxUse
+            browserPool.acquire(async() => {browserCount++; }),
+            delay
+        ]);
+        // await clearTimeout(timeoutId);
+    });
+    t.is(browserCount, 1);
+});
+
+test('acquired browser is not closed when testOnBorrow is true', async (t) => {
+    const browserPool = new BrowserPool({
+        browserMaxUse: 2,
+        poolSettings: { // options for generic pool
+            idleTimeoutMillis: 300000,
+            max: 1,
+            min: 1,
+            testOnBorrow: true,
+        },
+        browserArgs: {args: ['--no-sandbox']}
+    });
+
+    let browser = await browserPool.acquire(async(browser: Browser) => browser);
+    let pageReturned = await browser.newPage().catch(() => false);
+    t.truthy(pageReturned);
+
+    await browser.close();
+    // @ts-ignore
+    pageReturned = await browser.newPage().catch((error) => false);
+    t.is(pageReturned, false);
+
+
+    browser = await browserPool.acquire(async(browser: Browser) => browser);
+    pageReturned = await browser.newPage().catch(() => false);
+    t.truthy(pageReturned);
+});
+
