3f6ee40d95d0b387273743e64a941905eb4cd91d
Dave Smart <d.smart@smartinternetconsultants.co.uk>
2020-11-04 11:11:15 +0000

    Makes browser closing configurable = Adds `closeBrowser` to config

--- README.md
+++ README.md
@@ -183,6 +183,7 @@ root. Available configuration options:
  * `cache` _default `null`_ - set to `datastore` to enable caching on Google Cloud using datastore _only use if deploying to google cloud_, `memory` to enable in-memory caching or `filesystem` to enable disk based caching
  * `cacheConfig` - an object array to specify caching options
  * `renderOnly` - restrict the endpoint to only service requests for certain domains. Specified as an array of strings. eg. `['http://render.only.this.domain']`. This is a strict prefix match, so ensure you specify the exact protocols that will be used (eg. http, https).
+ * `closeBrowser`_default `false` - `true` forces the browser to close and reopen between each page render, some sites might need this to prevent URLs past the first one rendered returning null responses.
 
 #### cacheConfig
 * `cacheDurationMinutes` _default `1440`_ - set an expiry time in minues, defaults to 24 hours. Set to -1 to disable cache Expiration
--- src/renderer.ts
+++ src/renderer.ts
@@ -142,7 +142,9 @@ export class Renderer {
       // This should only occur when the page is about:blank. See
       // https://github.com/GoogleChrome/puppeteer/blob/v1.5.0/docs/api.md#pagegotourl-options.
       await page.close();
-      await this.browser.close();
+      if (this.config.closeBrowser) {
+        await this.browser.close();
+      }
       return { status: 400, customHeaders: new Map(), content: '' };
     }
 
@@ -150,7 +152,9 @@ export class Renderer {
     // https://cloud.google.com/compute/docs/storing-retrieving-metadata.
     if (response.headers()['metadata-flavor'] === 'Google') {
       await page.close();
-      await this.browser.close();
+      if (this.config.closeBrowser) {
+        await this.browser.close();
+      }
       return { status: 403, customHeaders: new Map(), content: '' };
     }
 
@@ -205,7 +209,9 @@ export class Renderer {
     const result = await page.content() as string;
 
     await page.close();
-    await this.browser.close();
+    if (this.config.closeBrowser) {
+      await this.browser.close();
+    }
     return { status: statusCode, customHeaders: customHeaders ? new Map(JSON.parse(customHeaders)) : new Map(), content: result };
   }
 
@@ -252,7 +258,9 @@ export class Renderer {
 
     if (!response) {
       await page.close();
-      await this.browser.close();
+      if (this.config.closeBrowser) {
+        await this.browser.close();
+      }
       throw new ScreenshotError('NoResponse');
     }
 
@@ -260,7 +268,9 @@ export class Renderer {
     // https://cloud.google.com/compute/docs/storing-retrieving-metadata.
     if (response!.headers()['metadata-flavor'] === 'Google') {
       await page.close();
-      await this.browser.close();
+      if (this.config.closeBrowser) {
+        await this.browser.close();
+      }
       throw new ScreenshotError('Forbidden');
     }
 
@@ -270,7 +280,9 @@ export class Renderer {
     // https://github.com/GoogleChrome/puppeteer/blob/v1.8.0/docs/api.md#pagescreenshotoptions
     const buffer = await page.screenshot(screenshotOptions) as Buffer;
     await page.close();
-    await this.browser.close();
+    if (this.config.closeBrowser) {
+      await this.browser.close();
+    }
     return buffer;
   }
 }
